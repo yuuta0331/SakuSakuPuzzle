40ae59507df562cd1240e3edd648d62fcf2d7633
yuuta0331 <miyagi.hiroshi@taupe.plala.or.jp>
2023-06-20 04:12:36 +0900

    Change of detailed ranking input scene

--- src/js/entities/keyboard.js
+++ src/js/entities/keyboard.js
@@ -1,5 +1,18 @@
 import {audio, Container, input, Renderable, timer, UITextButton} from "melonjs";
 
+function lerpColor(a, b, amount) {
+    var ah = parseInt(a.replace(/#/g, ''), 16),
+        ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+        bh = parseInt(b.replace(/#/g, ''), 16),
+        br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+        rr = ar + amount * (br - ar),
+        rg = ag + amount * (bg - ag),
+        rb = ab + amount * (bb - ab);
+
+    return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
+}
+
+
 class Keyboard extends Container {
     constructor(x, y) {
         super(x, y);
@@ -81,9 +94,48 @@ export class KeyButton extends UITextButton {
         //this.backgroundColor="#00aa0080"
     }
 
+    // 線形補間を行う関数
+    lerp(start, end, amt) {
+        return start * (1 - amt) + end * amt;
+    }
+
+// カラーコードをRGB値に変換する関数
+    hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
+// RGB値をカラーコードに変換する関数
+    rgbToHex(r, g, b) {
+        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+
+// // 2つの色を指定された比率で補間する関数
+//     lerpColor(color1, color2, amt) {
+//         var rgb1 = this.hexToRgb(color1);
+//         var rgb2 = this.hexToRgb(color2);
+//
+//         var r = Math.floor(this.lerp(rgb1.r, rgb2.r, amt));
+//         var g = Math.floor(this.lerp(rgb1.g, rgb2.g, amt));
+//         var b = Math.floor(this.lerp(rgb1.b, rgb2.b, amt));
+//
+//         return this.rgbToHex(r, g, b);
+//     }
+
     update(dt) {
         // 描画が更新されない問題の対策
         this.keyboard.toggleUppercase();
+        // if (this.keyboard.charset === VirtualKeyboard.CHARSET.KATAKANA ||
+        //     this.keyboard.charset === VirtualKeyboard.CHARSET.HIRAGANA) {
+        //     this.pressTime = timer.getTime();
+        // }
+        // if (timer.getTime() - this.pressTime > 10) {
+        //     this.keyboard.setOpacity(0.1);
+        // }
 
         if (input.isKeyPressed(this.character)) {
             this.pressTime = timer.getTime();  // キーが押された時刻を記録
@@ -96,6 +148,10 @@ export class KeyButton extends UITextButton {
             this.backgroundColor = "#f5b1b1"
             //this.isPressed = false;  // ボタンが押されているフラグを落とす
         }
+
+        // 現在時刻に対してsin関数を適用し、0から1の範囲で周期的に変化する値を作る
+        var amt = (Math.sin(timer.getTime() / 1000) + 1) / 2;  // 0から1の範囲で変化
+        this.backgroundColor = lerpColor("#fd21b1", "#ff6161", amt);
 
         return super.update(dt);
     }
@@ -150,6 +206,7 @@ class ToggleButton extends UITextButton {
         audio.play("enter");
         // 大文字と小文字の切り替え
         this.keyboard.toggleUppercase();
+        this.bitmapText.setText(this.keyboard.isUppercase ? "aA" : "Aa")
         return true;
     }
 
@@ -176,6 +233,8 @@ class QwertyButton extends UITextButton {
             font: "funwari-round_white",
             text: "Qwerty",
             borderHeight: 80,
+            textAlign: "center",
+            textBaseline: "middle",
             backgroundColor: "#f5dab1",
             hoverColor: "#ffc061",
         };
@@ -186,12 +245,53 @@ class QwertyButton extends UITextButton {
         // BitmapTextの位置を調整
         this.bitmapText.centerY = 27.0;
         this.keyboard = keyboard;
+
+        if (this.keyboard.charset === VirtualKeyboard.CHARSET.ALPHABET) {
+            this.bitmapText.setText("Qwerty");
+        } else if (this.keyboard.charset === VirtualKeyboard.CHARSET.QWERTY) {
+            this.border.width = 200;
+            this.bitmapText.centerX = 100;
+            this.bitmapText.setText('ABC');
+        }
     }
 
     onClick(event) {
         audio.play("enter");
-        this.keyboard.isQwerty = !this.keyboard.isQwerty;
-        this.keyboard.setupKeyboard();
+        //this.keyboard.isQwerty = !this.keyboard.isQwerty;
+        this.keyboard.toggleQwerty();
+
+        return true;
+    }
+}
+
+class KanaButton extends UITextButton {
+    constructor(x, y, keyboard) {
+        let settings = {
+            font: "funwari-round_white",
+            text: "かな",
+            borderHeight: 80,
+            backgroundColor: "#f5dab1",
+            hoverColor: "#ffc061",
+        };
+        super(x, y, settings);
+        // MelonJSのバグでBitMapTextが0,0にも描画されるので、位置を修正する
+        //this.bitmapText = new BitmapText(x, y, settings);
+        //this.bitmapText.pos.set(x, y);
+        // BitmapTextの位置を調整
+        this.bitmapText.centerY = 30.0;
+        this.keyboard = keyboard;
+
+        if (this.keyboard.charset === VirtualKeyboard.CHARSET.HIRAGANA) {
+            this.bitmapText.setText("カナ")
+        } else if (this.keyboard.charset === VirtualKeyboard.CHARSET.KATAKANA) {
+            this.bitmapText.setText("かな")
+        }
+    }
+
+    onClick(event) {
+        audio.play("enter");
+        this.keyboard.toggleKana();
+
         return true;
     }
 }
@@ -268,8 +368,15 @@ class ConfirmButton extends UITextButton {
     }
 }
 
-
 export default class VirtualKeyboard extends Container {
+
+    static CHARSET = {
+        ALPHABET: 0,
+        QWERTY: 1,
+        HIRAGANA: 2,
+        KATAKANA: 3
+    };
+
     constructor(x, y, parent) {
         super(x, y);
 
@@ -280,29 +387,22 @@ export default class VirtualKeyboard extends Container {
         this.isUppercase = this.parent.isUppercase;
         this.isQwerty = false;
 
+// 初期化のとき
+        this.charset = VirtualKeyboard.CHARSET.ALPHABET;
 
         this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
         this.qwerty = 'qwertyuiopasdfghjklzxcvbnm';
         this.numbers = '0123456789';
-    }
+        this.hiragana = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん';
+        this.katakana = 'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン';
 
-    // setupKeyboard = () => {
-    //     // 全てのキーボタンを削除する
-    //     game.world.children.filter(child => child instanceof KeyButton).forEach(child => game.world.removeChild(child));
-    //
-    //     // 新たなキーボタンを作成する
-    //     const keys = this.isQwerty ? this.qwerty : this.alphabet;
-    //
-    //     for(let i = 0; i < this.numbers.length; i++) {
-    //         this.addChild(new KeyButton(this.pos.x + (i * 100), this.pos.y + 0, this.numbers[i], this));
-    //     }
-    //     for(let i = 0; i < 13; i++) {
-    //         this.addChild(new KeyButton(this.pos.x + (i * 100), this.pos.y + 100, keys[i], this));
-    //     }
-    //     for(let i = 13; i < keys.length; i++) {
-    //         this.addChild(new KeyButton(this.pos.x + ((i - 13) * 100), this.pos.y + 200, keys[i], this));
-    //     }
-    // }
+        // キーボードのレイアウトを作成
+        this.hiraganaBig = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん';
+        this.hiraganaSmall = 'ぁぃぅぇぉっゃゅょゎ';
+        this.katakanaBig = 'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン';
+        this.katakanaSmall = 'ァィゥェォッャュョヮ';
+
+    }
 
     onActivateEvent() {
         // キーボードを表示する
@@ -310,37 +410,62 @@ export default class VirtualKeyboard extends Container {
     }
 
     setupKeyboard() {
-        //game.world.children.filter(child => child instanceof KeyButton).forEach(child => game.world.removeChild(child));
-        //this.children.filter(child => child instanceof KeyButton).forEach(child => this.removeChildNow(child));
         this.children.filter(child => child instanceof UITextButton).forEach(child => this.removeChildNow(child));
 
-        const keys = this.isQwerty ? this.qwerty : this.alphabet;
+        // charsetの値に基づいて現在のcharsetを選択する
+        let charset;
+        switch (this.charset) {
+            case 0:
+                charset = this.alphabet;
+                break;
+            case 1:
+                charset = this.qwerty;
+                break;
+            case 2:
+                charset = this.hiragana;
+                //charset = this.hiraganaBig + this.hiraganaSmall;
+                break;
+            case 3:
+                charset = this.katakana;
+                //charset = this.katakanaBig + this.katakanaSmall;
+                break;
+        }
 
         let keyWidth = 100;
         let keyHeight = 100;
 
-        // Create a separate array for each row of keys
-        const rows = this.isQwerty ? [
-            this.qwerty.slice(0, 10),  // Numbers
-            this.qwerty.slice(10, 19), // Q to P
-            this.qwerty.slice(19, 28), // A to L
-            this.qwerty.slice(28)      // Z to M
-        ] : [
-            this.alphabet.slice(0, this.alphabet.length / 2),
-            this.alphabet.slice(this.alphabet.length / 2),
-        ];
+        // レイアウトの選択
+        let layout;
+        if (this.charset == 0) {  // ABC配列
+            layout = [
+                charset.slice(0, charset.length / 2),
+                charset.slice(charset.length / 2),
+            ];
+        } else if (this.charset == 1) {  // QWERTY配列
+            layout = [
+                charset.slice(0, 10),  // Numbers
+                charset.slice(10, 19), // Q to P
+                charset.slice(19, 28), // A to L
+                charset.slice(28)      // Z to M
+            ];
+        } else {  // ひらがなとカタカナ
+            layout = [
+                charset.slice(0, 10),
+                charset.slice(10, 20),
+                charset.slice(20, 30),
+                charset.slice(30),
 
-        let totalWidth = Math.max(...rows.map(row => row.length)) * keyWidth;
-        //let totalHeight = (rows.length + this.isQwerty ? 3 : 3) * keyHeight;
-        //let totalHeight = (this.isQwerty ? 3 : 2) * keyHeight;
-        let totalHeight = 3 * keyHeight;
+                // 小文字も入れた場合
+                // charset.slice(30, 40),
+                // charset.slice(40),
+            ];
+        }
+
+        let totalWidth = Math.max(...layout.map(row => row.length)) * keyWidth;
+        let totalHeight = layout.length * keyHeight;
 
         let startX = this.pos.x - totalWidth / 2;
         let startY = this.pos.y - totalHeight / 2;
-// If Qwerty layout is active, move the startY up to accommodate the additional row
-//         if (this.isQwerty) {
-//             startY += keyHeight;
-//         }
 
         // Add number keys
         for (let i = 0; i < this.numbers.length; i++) {
@@ -348,8 +473,8 @@ export default class VirtualKeyboard extends Container {
         }
 
         // Add letter keys
-        for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-            let row = rows[rowIndex];
+        for (let rowIndex = 0; rowIndex < layout.length; rowIndex++) {
+            let row = layout[rowIndex];
             let rowStartX = startX + ((totalWidth - row.length * keyWidth) / 2);
             for (let i = 0; i < row.length; i++) {
                 this.addChild(new KeyButton(rowStartX + (i * keyWidth), startY + ((rowIndex + 1) * keyHeight), row[i], this));
@@ -359,10 +484,12 @@ export default class VirtualKeyboard extends Container {
         // Add control buttons
         let buttonWidth = 200;
         let buttonHeight = 100;
-        this.addChild(new ToggleButton(startX, startY + totalHeight + buttonHeight, this));
-        this.addChild(new QwertyButton(startX + buttonWidth, startY + totalHeight + buttonHeight, this));
-        this.addChild(new BackspaceButton(startX + totalWidth - buttonWidth, startY + totalHeight + buttonHeight, this));
-        this.addChild(new ConfirmButton(startX + totalWidth - buttonWidth, startY + totalHeight + 2 * buttonHeight, this));
+        let buttonStartY = startY + totalHeight + buttonHeight - (this.charset == 1 ? keyHeight : 0);
+        this.addChild(new ToggleButton(startX, buttonStartY, this));
+        this.addChild(new QwertyButton(startX + buttonWidth, buttonStartY, this));
+        this.addChild(new KanaButton(startX + 3 * buttonWidth, buttonStartY, this));
+        this.addChild(new BackspaceButton(startX + totalWidth - buttonWidth, buttonStartY, this));
+        this.addChild(new ConfirmButton(startX + totalWidth - buttonWidth, buttonStartY + buttonHeight, this));
     }
 
 
@@ -372,4 +499,50 @@ export default class VirtualKeyboard extends Container {
         this.isUppercase = this.parent.isUppercase;
         this.children.filter(child => child instanceof KeyButton).forEach(KeyButton => KeyButton.updateCharacter());
     }
+
+
+    // アルファベットとひらがなとカタカナを切り替えるメソッドを追加
+    toggleCharset() {
+        // if (this.currentCharset === this.alphabet) {
+        //     this.currentCharset = this.hiragana;
+        // } else if (this.currentCharset === this.hiragana) {
+        //     this.currentCharset = this.katakana;
+        // } else {
+        //     this.currentCharset = this.alphabet;
+        // }
+        //
+        // // キーボードを再セットアップする
+        // this.setupKeyboard();
+
+        this.charset = (this.charset + 1) % 4;  // charsetを次の値に変更する
+        this.setupKeyboard();  // キーボードを再セットアップする
+    }
+
+    toggleQwerty() {
+        if (this.charset === VirtualKeyboard.CHARSET.ALPHABET) {
+            this.charset = VirtualKeyboard.CHARSET.QWERTY;
+        } else if (this.charset === VirtualKeyboard.CHARSET.QWERTY) {
+            this.charset = VirtualKeyboard.CHARSET.ALPHABET;
+        } else {
+            this.charset = VirtualKeyboard.CHARSET.ALPHABET;
+        }
+
+        // キーボードを再セットアップする
+        this.setupKeyboard();
+    }
+
+    toggleKana() {
+        if (this.charset === VirtualKeyboard.CHARSET.HIRAGANA) {
+            this.charset = VirtualKeyboard.CHARSET.KATAKANA;
+        } else if (this.charset === VirtualKeyboard.CHARSET.KATAKANA) {
+            this.charset = VirtualKeyboard.CHARSET.HIRAGANA;
+        } else {
+            this.charset = VirtualKeyboard.CHARSET.HIRAGANA;
+        }
+
+        // キーボードを再セットアップする
+        this.setupKeyboard();
+    }
+
+
 }
--- src/js/stage/ranking_input.js
+++ src/js/stage/ranking_input.js
@@ -12,6 +12,45 @@ import {
 import g_game from "../../game";
 import VirtualKeyboard from "../entities/keyboard";
 
+
+// 入力フィールド
+class CenteredText extends Text {
+    constructor(x, y, settings) {
+        super(x, y, settings);
+
+        this.text = settings.text;
+        this.pos.x = (game.viewport.width / 2) - (this.text.length * this.fontSize / 2) - 400;
+    }
+
+    // setText method をオーバーライドして中央揃えを可能にする
+    setText(text) {
+        super.setText(text);
+        this.text = text;
+        // Update the position to keep the text centered
+        //this.pos.x = (game.viewport.width / 2) - (text.length * this.fontSize / 2);
+    }
+
+    update(dt) {
+        this.pos.x = (game.viewport.width / 2) - (this.text.length * this.fontSize / 2);
+        return super.update(dt);
+    }
+}
+
+// 角丸長方形
+class RoundedRect extends Renderable {
+    constructor(x, y, w, h, radius) {
+        super(x, y, w, h);
+        this.fillColor = "rgba(255, 255, 255, 0.5)";  // 半透明の白色の背景
+        this.radius = radius;  // 角の半径
+    }
+
+    draw(renderer) {
+        renderer.setColor(this.fillColor);
+        renderer.fillRoundRect(this.pos.x, this.pos.y, this.width, this.height, this.radius);
+    }
+}
+
+
 class DebugRect extends Renderable {
     constructor(x, y, w, h) {
         super(x, y, w, h);
@@ -32,6 +71,8 @@ class DebugRect extends Renderable {
 
 
 class RankingInputScreen extends Stage {
+
+    static MAX_INPUT_LENGTH = 20;
 
     onResetEvent() {
 
@@ -57,7 +98,7 @@ class RankingInputScreen extends Stage {
         //     text: "Ranking"
         // }));
 
-        let guidText = new Text(game.viewport.width / 2, game.viewport.height * 0.8, {
+        let guidText = new Text(game.viewport.width / 2, game.viewport.height * 0.9, {
             font: "funwari_round",
             size: 60,
             textBaseline: "middle",
@@ -73,6 +114,7 @@ class RankingInputScreen extends Stage {
         input.bindKey(input.KEY.BACKSPACE, "back");
         input.bindKey(input.KEY.B, "back");
         input.bindKey(input.KEY.SHIFT, "shift");
+        input.bindKey(input.KEY.TAB, "tab");
 
         // ゲームパッドのボタンをキーボードのキーにマッピング
         input.bindGamepad(0, {type: "buttons", code: input.GAMEPAD.BUTTONS.FACE_1}, input.KEY.ENTER);
@@ -86,13 +128,32 @@ class RankingInputScreen extends Stage {
         this.userInput = "";
 
         // 入力フィールドを表示するためのテキストオブジェクト
-        this.inputField = new Text(game.viewport.width / 2, game.viewport.height * 0.3, {
+        this.inputField = new Text(game.viewport.width / 2, game.viewport.height * 0.2, {
             font: "funwari_round",
-            size: 60,
-            text: 'input' + this.userInput,
-            fillStyle: '#FFFFFF'
+            size: 90,
+            textBaseline: "middle",
+            textAlign: "center",
+            text: 'おなまえをにゅうりょく',
+            fillStyle: '#ff6767'
         });
         game.world.addChild(this.inputField, 3);
+        this.inputBox = new RoundedRect(this.inputField.pos.x, this.inputField.pos.y, this.inputField.fontSize * RankingInputScreen.MAX_INPUT_LENGTH, this.inputField.height + 50, 20);
+        game.world.addChild(this.inputBox, 2);
+
+
+        // テンキーのキーコード（96-105）
+        for (let i = 96; i <= 105; i++) {
+            // テンキーのキーコードと対応する数字
+            let char = String.fromCharCode(i - 48);
+            input.bindKey(i, char);
+        }
+
+
+        // ASCII values for 0-9
+        for (let i = 48; i <= 57; i++) {
+            input.bindKey(i, String.fromCharCode(i));
+        }
+
 
         // AからZまでのキーを入力としてバインド
         for (let i = 65; i <= 90; i++) {  // ASCII values for A-Z
@@ -104,8 +165,11 @@ class RankingInputScreen extends Stage {
 
     // KeyButtonから呼ばれるメソッド
     addCharacterToInput(char) {
-        this.userInput += char;
-        this.inputField.setText(this.userInput);
+        // 文字数制限を設けるための定数
+        if (this.userInput.length < RankingInputScreen.MAX_INPUT_LENGTH) {
+            this.userInput += char;
+            this.inputField.setText(this.userInput);
+        }
     }
 
     removeLastCharacterFromInput() {
@@ -143,26 +207,57 @@ class RankingInputScreen extends Stage {
         let delay = 200;  // キー入力の遅延時間（ミリ秒）
 
         // AからZまでのキーが押されたら、その文字を入力フィールドに追加
-        for (let i = 65; i <= 90; i++) {
-            let char = String.fromCharCode(i).toLowerCase();
+        // A-Zのキーと0-9のキーに対応
+        // for (let i = 48; i <= 90; i++) {
+        //     // 数字のASCII範囲(58-64)は除外
+        //     if (i > 57 && i < 65) continue;
+        //
+        //     let char = String.fromCharCode(i).toLowerCase();
+        //     if (input.isKeyPressed(char)) {
+        //         // 前回の入力から一定時間が経過していれば、入力を受け付ける
+        //         if (!this.lastInputTime[char] || Date.now() - this.lastInputTime[char] > delay) {
+        //             audio.play("enter");
+        //             // 数字か小文字のアルファベットの場合はそのまま、大文字のアルファベットの場合はisUppercaseに基づいて変換
+        //             let output = (i >= 65 && this.isUppercase) ? char.toUpperCase() : char;
+        //             this.userInput += output;
+        //             this.inputField.setText(this.userInput);
+        //             this.lastInputTime[char] = Date.now();
+        //         }
+        //     }
+        // }
+
+        // 常に中央揃えになるように更新
+        this.inputField.autoTransform = true;
+        this.inputField.updateTransform = function () {
+            this.pos.x = (game.viewport.width / 2) - (this.width / 2);
+            Renderable.prototype.updateTransform.apply(this, arguments);
+        }
+
+        // A-Zのキー、0-9のキー、テンキーに対応
+        for (let i = 48; i <= 105; i++) {
+            // 数字のASCII範囲(58-64)とテンキーのキーコード範囲（96-105）以外は除外
+            if ((i > 57 && i < 65) || (i > 90 && i < 96)) continue;
+
+            let char = i > 90 ? String.fromCharCode(i - 48) : String.fromCharCode(i).toLowerCase();
+
             if (input.isKeyPressed(char)) {
                 // 前回の入力から一定時間が経過していれば、入力を受け付ける
                 if (!this.lastInputTime[char] || Date.now() - this.lastInputTime[char] > delay) {
                     audio.play("enter");
-                    let output = this.isUppercase ? char.toUpperCase() : char;
-                    this.userInput += output;
-                    this.inputField.setText(this.userInput);
+                    // 数字か小文字のアルファベットの場合はそのまま、大文字のアルファベットの場合はisUppercaseに基づいて変換
+                    let output = (i >= 65 && i <= 90 && this.isUppercase) ? char.toUpperCase() : char;
+                    this.addCharacterToInput(output);
                     this.lastInputTime[char] = Date.now();
                 }
             }
         }
 
+
         // バックスペースキーが押されたら、最後の文字を削除する
         if (input.isKeyPressed('back')) {
             if (!this.lastInputTime['back'] || Date.now() - this.lastInputTime['back'] > delay) {
                 audio.play("return");
-                this.userInput = this.userInput.slice(0, -1);
-                this.inputField.setText(this.userInput);
+                this.removeLastCharacterFromInput();
                 this.lastInputTime['back'] = Date.now();
             }
         }
@@ -170,8 +265,18 @@ class RankingInputScreen extends Stage {
         if (input.isKeyPressed('shift')) {
             if (!this.lastInputTime['shift'] || Date.now() - this.lastInputTime['shift'] > delay) {
                 //this.virtualKeyboard.isUppercase = !this.virtualKeyboard.isUppercase;
+                audio.play("enter");
                 this.virtualKeyboard.toggleUppercase();
                 this.lastInputTime['shift'] = Date.now();
+            }
+        }
+
+        if (input.isKeyPressed('tab')) {
+            if (!this.lastInputTime['tab'] || Date.now() - this.lastInputTime['tab'] > delay) {
+                //this.virtualKeyboard.isUppercase = !this.virtualKeyboard.isUppercase;
+                audio.play("enter");
+                this.virtualKeyboard.toggleCharset();
+                this.lastInputTime['tab'] = Date.now();
             }
         }
 
